fa6ccc1d33e08eb0732e28a0f7380309c20770fb
Bruce <meabru@gmail.com>
2019-06-22 01:15:05 -0500

    Fix a couple small build issues

--- yomo/Navigation/Locomotion.cs
+++ yomo/Navigation/Locomotion.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using PidController;
-using Unosquare.RaspberryIO;
 using Unosquare.RaspberryIO.Abstractions;
-using Unosquare.RaspberryIO.Native;
-using Unosquare.WiringPi;
 
-namespace yomo
+namespace yomo.Navigation
 {
 	public class Locomotion
 	{
@@ -13,7 +9,7 @@ namespace yomo
         Wheel left = new Wheel(BcmPin.Gpio13);
         Wheel right = new Wheel(BcmPin.Gpio18);
 
-        private PidController.PidController headingPid = new PidController.PidController(1.1, 0.1, 0.1, 180, -180);
+        private PidController headingPid = new PidController(1.1, 0.1, 0.1, 180, -180);
 
         float idealHeading;
         float correctingHeading;
@@ -56,35 +52,5 @@ namespace yomo
             //SteerWheels();
         }
 	}
-
-    public class Wheel
-    {
-        int speed;
-        GpioPin pin;
-
-        public Wheel(BcmPin gpioPin)
-        {
-            // TODO: Check out:
-            // https://raspberrypi.stackexchange.com/questions/4906/control-hardware-pwm-frequency
-            // https://stackoverflow.com/questions/20081286/controlling-a-servo-with-raspberry-pi-using-the-hardware-pwm-with-wiringpi
-
-            pin = (GpioPin)Pi.Gpio[gpioPin];
-            pin.PinMode = GpioPinDriveMode.PwmOutput;
-            pin.PwmMode = PwmMode.MarkSign;
-
-            // pwmFrequency in Hz = 19.2e6 Hz / pwmClock / pwmRange.
-            pin.PwmClockDivisor = 16; // 1 is 4096, possible values are all powers of 2 starting from 2 to 2048
-            pin.PwmRange = 1024; 
-            pin.PwmRegister = 0; // (int)(pin.PwmRange * decimal duty); // This goes from 0 to PwmRange-1
-        }
-
-        public void SetSpeed(int speed)
-        {
-            var inRange = (uint)Math.Max(0, Math.Min(MaxSpeed, speed));
-            pin.PwmRegister = (int)(MaxSpeed / inRange);
-        }
-
-        public uint MaxSpeed { get { return pin.PwmRange; } }
-    }
 }
 
