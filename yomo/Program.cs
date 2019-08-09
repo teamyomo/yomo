affde4e199ff4ec2be0f0d8f25140f9ae64a5d79
Bruce <meabru@gmail.com>
2019-08-09 16:48:33 -0500

    minor tweak

--- yomo/Program.cs
+++ yomo/Program.cs
@@ -20,6 +20,7 @@ namespace yomo
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseUrls("http://0.0.0.0");
+                .UseUrls("http://0.0.0.0")
+                ;
     }
 }
